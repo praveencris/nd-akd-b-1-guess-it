c9df2b0bc28272e22dadeb4e600bdf219c129e21
Praveen <pkchmits@gmail.com>
2020-12-17 07:47:50 +0530

    Added lifecycleOwner for databinding 1. Learnt how to get rid of .observe using xml databinding expressions

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -58,15 +58,12 @@ class ScoreFragment : Fragment() {
                 .get(ScoreViewModel::class.java)
 
         binding.scoreViewModel = viewModel
-        // TODO (05) Call binding.setLifecycleOwner and remove the score observer
+        // DONE (05) Call binding.setLifecycleOwner and remove the score observer
 
-        // Add observer for score
-        viewModel.score.observe(this, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
+        binding.setLifecycleOwner(this)
 
         // Navigates back to title when button is pressed
-        viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
             if (playAgain) {
                 findNavController().navigate(ScoreFragmentDirections.actionRestart())
                 viewModel.onPlayAgainComplete()
